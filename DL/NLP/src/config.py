582e2cc48cdb2ad94fd8a6eb2bc8fada973149ef
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2024-04-21 20:08:44 +0530

    Update config.py

--- DL/NLP/src/config.py
+++ DL/NLP/src/config.py
@@ -1,1 +1,16 @@
 
+import transformers
+
+MAX_LEN = 128
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 8
+EPOCHS = 10
+BASE_MODEL_PATH = ".../bertModel/input/bert_base_uncased"
+MODEL_PATH = "model.bin"    # model saving path -- will be saved in src folder
+TRAINING_FILE = ".../bertModel/input/ner_dataset.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    
+    BASE_MODEL_PATH,
+    do_lower_case = True
+    
+)
