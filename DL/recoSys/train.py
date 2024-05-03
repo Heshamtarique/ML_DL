bc2fedfb6a12a3f6291bb88406fe9ef93c2922f1
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2024-05-03 11:38:31 +0530

    Update train.py

--- DL/recoSys/train.py
+++ DL/recoSys/train.py
@@ -1,1 +1,26 @@
+import tez
+import pandas as pd
+from sklearn import model_selection
 
+
+class MovieDataset:
+    def __init__(self, users, movies, ratings):
+        self.users= users
+        self.movies = movies
+        self.ratings = ratings
+        
+        
+
+    def __len__(self):
+        return len(self.users)
+
+
+def train():
+    df = pd.read_csv(".../user/recoSys/train.csv")
+
+    # ID user movie ratinf
+    df_train, df_valid = model_selection.train_test_split(df, test_size=0.1,
+                                                          random_state=42,
+                                                          stratify=df.rating.values)
+    ...
+    
