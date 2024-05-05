4c6979ff60209f1a5a30b81c35175bfe1c26ae79
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2024-05-05 23:13:13 +0530

    Update dataset.py

--- DL/recoSys/dataset.py
+++ DL/recoSys/dataset.py
@@ -1,1 +1,26 @@
+# building datapipeline
+
+class MovieDataset:
+  
+    def __init__(self, users, movies, ratings):
+        
+        # these features we saw in the dataaset... explore the features !
+        self.users= users
+        self.movies = movies
+        self.ratings = ratings
+
+    def __len__(self):
+        return len(self.users)   
+      # how many samples we have 
+
+    def __getitem__(self, item):
+        user = self.users[item]
+        movie = self.movies[item]
+        rating = self.ratings[item]
+        
+        return {
+            "user": torch.tensor(user, dtype = torch.long),
+            "movie": torch.tensor(movie, dtype = torch.long ),
+            "rating": torch.tensor( rating, dtype=torch.float)    # rating could be float
+        }
 
