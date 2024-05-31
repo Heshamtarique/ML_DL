09dbd3f07e68d213bea5726032d21ab2d60c7e15
Hesham Mohammad Tarique <63514840+Heshamtarique@users.noreply.github.com>
2024-05-31 22:59:43 +0530

    Update main.py

--- DL/Computer_Vision/text_extraction_service/main.py
+++ DL/Computer_Vision/text_extraction_service/main.py
@@ -1,1 +1,14 @@
-
+class PDFTextExtractor:
+    def __init__(self, directory):   # essential to pass as args
+        self.directory = directory
+        self.extracted_text = {}
+        
+    
+    def clean_text(self, text):
+        try:
+            text = re.sub(r'\s+', ' ', text)# multiple spaces in data wit single space
+            text = re.sub(r'\n', ' ', text)  # new line with one space
+            return text.strip()
+        except Exception as e:
+            print(f"Error1: {e}")
+         
